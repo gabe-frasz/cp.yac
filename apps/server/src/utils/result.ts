84180d6bd6398ef0097925de8a0894924fd71fa6
Gabriel Frasão <gabrielvitor.frasao@gmail.com>
2025-05-18 01:43:38 -0300

    ok function data can be null

--- apps/server/src/utils/result.ts
+++ apps/server/src/utils/result.ts
@@ -1,7 +1,7 @@
 export type Result<T, E> = [T, null] | [null, E];
 
-export function ok<T>(data: T): Result<T, null> {
-  return [data, null];
+export function ok<T>(data?: T): Result<T, null> {
+  return data ? [data, null] : [null, null];
 }
 
 export function err<E>(error: E): Result<null, E> {
