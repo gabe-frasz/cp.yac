a4d41211e13949e2b419b3dcfd6db08fb4e3e36f
Gabriel Frasão <gabrielvitor.frasao@gmail.com>
2025-05-18 01:25:35 -0300

    create helper functions to use result type

--- apps/server/src/utils/result.ts
+++ apps/server/src/utils/result.ts
@@ -1,1 +1,9 @@
 export type Result<T, E> = [T, null] | [null, E];
+
+export function ok<T>(data: T): Result<T, null> {
+  return [value, null];
+}
+
+export function err<E>(error: E): Result<null, E> {
+  return [null, error];
+}
