0ba1696b21e4e97d22a1445e9f18afdac0539e72
Gabriel Frasão <gabrielvitor.frasao@gmail.com>
2025-05-19 11:59:33 -0300

    create message entity

---
+++ apps/server/src/app/entities/message.ts
@@ -0,0 +1,40 @@
+import { z } from "zod";
+import { ulid } from "ulid";
+
+import { AI_AGENT_CHAT_ID } from "@/constants";
+
+const messageSchema = z.object({
+  id: z.ulid().default(ulid),
+  senderId: z.union([z.literal(AI_AGENT_CHAT_ID), z.ulid()]),
+  text: z.string(),
+});
+
+type MessageProps = z.output<typeof messageSchema>;
+type MessageConstructorProps = z.input<typeof messageSchema>;
+
+export class Message {
+  private props: MessageProps;
+
+  constructor(props: MessageConstructorProps) {
+    this.props = this.validate(props);
+  }
+
+  get id() {
+    return this.props.id;
+  }
+
+  get senderId() {
+    return this.props.senderId;
+  }
+
+  get text() {
+    return this.props.text;
+  }
+
+  private validate(props: MessageConstructorProps) {
+    const { success, error, data } = messageSchema.safeParse(props);
+    if (!success) throw new Error(z.prettifyError(error));
+
+    return data;
+  }
+}
