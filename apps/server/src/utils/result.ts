c91a8a0c643da19e0acce0b0e8c0c966bdf675bf
Gabriel Frasão <gabrielvitor.frasao@gmail.com>
2025-05-19 10:28:51 -0300

    solve type issues with external function using ok and err helpers

--- apps/server/src/utils/result.ts
+++ apps/server/src/utils/result.ts
@@ -1,9 +1,9 @@
 export type Result<T, E> = [T, null] | [null, E];
 
-export function ok<T>(data?: T): Result<T, null> {
+export function ok<T>(data?: T): Result<T, any> {
   return data ? [data, null] : [null, null];
 }
 
-export function err<E>(error: E): Result<null, E> {
+export function err<E>(error: E): Result<any, E> {
   return [null, error];
 }
